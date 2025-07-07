2c8ecf27847be1b493f819b36b640c8dee6d398d
Mikalai Kutouski <mkutouski@opennebula.io>
2025-07-07 16:37:45 +0300

    M #-: More strict UUID pattern for
    Signed-off-by: Mikalai Kutouski <mkutouski@opennebula.io>

--- src/models/appliances.rb
+++ src/models/appliances.rb
@@ -13,7 +13,8 @@ class Appliances
 
     def reload
         if File.directory?(@dir)
-            ptrn = @dir + '/appliances/**/*.yaml'
+             # Match only files where the filename is a valid UUID, followed by .yaml extension
+             ptrn = @dir + '/appliances/**/????????-????-????-????-????????????.yaml'
 #        elsif File.file?(@dir)
 #            ptrn = @dir
         else
